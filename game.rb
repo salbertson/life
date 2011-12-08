bd5d3495d3ec67b6cd020a62f7ea7c9dbce89456
Scott Albertson <scott.onix@gmail.com>
2011-12-07 20:12:18 -0800

    Add #neighbor_count.

--- game.rb
+++ game.rb
@@ -6,4 +6,30 @@ class Game
       Array.new(columns)
     end
   end
+
+  def print
+    @board.length.times do |row|
+      puts @board[row].inspect
+    end
+  end
+
+  def neighbor_count(x, y)
+    count = 0
+
+    [-1, 0, 1].each do |y_offset|
+      [-1, 0, 1].each do |x_offset|
+        neighbor_y = y + y_offset
+        neighbor_x = x + x_offset
+
+        if neighbor_x >= 0 && neighbor_y >= 0 && @board[neighbor_y] && @board[neighbor_y][neighbor_x]
+          #puts "(#{neighbor_x}, #{neighbor_y})"
+          if y_offset != 0 || x_offset != 0
+            count += 1 if @board[neighbor_y][neighbor_x]
+          end
+        end
+      end
+    end
+
+    count
+  end
 end
--- game_test.rb
+++ game_test.rb
@@ -6,4 +6,15 @@ class GameTest < Test::Unit::TestCase
     game = Game.new(2, 3)
     assert_equal [[nil, nil, nil],[nil, nil, nil]], game.board
   end
+
+  def test_neighbor_count
+    game = Game.new(3, 3)
+    game.board[0][1] = 1
+    game.board[1][1] = 1
+    game.board[2][1] = 1
+
+    assert_equal 2, game.neighbor_count(0, 0)
+    assert_equal 1, game.neighbor_count(1, 0)
+    assert_equal 2, game.neighbor_count(1, 1)
+  end
 end
