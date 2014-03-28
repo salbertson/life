2a0bc23561d1865ea9d6f5ed60b094d7f190e4c2
Scott Albertson <scott@thoughtbot.com>
2014-03-28 15:13:35 -0700

    Make some changes

--- game.rb
+++ game.rb
@@ -6,9 +6,9 @@ class Game
 
   attr_accessor :board
 
-  def initialize(rows, columns)
+  def initialize rows, columns
     @board = Array.new(rows) do
-      Array.new(columns, 0)     
+      Array.new(columns, 0)
     end
   end
 
