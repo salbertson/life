7a6e4ecc578890676b7d15d2c63b1605e9810c29
Scott Albertson <ascottalbertson@gmail.com>
2015-10-08 14:09:48 -0700

    Add trailing whitespace

--- game.rb
+++ game.rb
@@ -4,7 +4,7 @@ class Game
   # - Allow cells to "loop" the board by calculating neighbor count with modulus
   # - Play endlessly, dump out board each iteration, and clear previous output
 
-  attr_accessor :board
+  attr_accessor :board                          
 
   def initialize(rows, columns)
     @board = Array.new(rows) do
