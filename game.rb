8788f69e65317c9ea7f0019e81b920ee90ae43c0
Scott Albertson <ascottalbertson@gmail.com>
2015-10-14 07:46:14 -0700

    Break Hound?

--- game.rb
+++ game.rb
@@ -4,7 +4,7 @@ class Game
   # - Allow cells to "loop" the board by calculating neighbor count with modulus
   # - Play endlessly, dump out board each iteration, and clear previous output
 
-  attr_accessor :board
+  attr_accessor :board            
 
   def initialize(rows, columns)
     @board = Array.new(rows) do
@@ -13,6 +13,7 @@ class Game
   end
 
   def neighbor_count(x, y)
+    debugger
     count = 0
 
     [-1, 0, 1].each do |y_offset|
@@ -30,6 +31,7 @@ class Game
   end
 
   def play()
+    debugger
     temporary_board = Marshal.load(Marshal.dump(@board))
 
     @board.length.times do |row|
